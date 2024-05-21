82b276458e16a14f40b890c9f18e9b72d300713e
chriswoodard7 <cmwoodard12@gmail.com>
2024-05-21 15:15:07 -0500

    commit

--- src/imu_mouse.c
+++ src/imu_mouse.c
@@ -3,6 +3,9 @@
 #include "pico/stdio_usb.h"
 #include "hardware/i2c.h"
 #include "pico/binary_info.h"
+#include "/pico-sdk/lib/tinyusb/hw/bsp/rp2040/board.h"
+#include "/pico-sdk/lib/tinyusb/src/tusb.h"
+#include "usb_descriptors.h"
 
 // device address
 #define ADDR _u(0b1101000)
@@ -33,7 +36,8 @@
 
 // function prototypes
 void chip_init();
-void read_sensor();
+void read_sensor(float *data_array);
+static void send_hid_report(uint8_t report_id, float xaccel);
 
 // hardware registers
 int main()
@@ -72,6 +76,10 @@ int main()
     printf("Y-Gyro Value: %.2f degrees/s\r\n", data_array[5]);
     printf("Z-Gyro Value: %.2f degrees/s\r\n", data_array[6]);
     printf("Temperature: %.2f C\r\n", data_array[3]);
+
+    float xaccel = data_array[0];
+
+    send_hid_report(REPORT_ID_KEYBOARD, xaccel);
     sleep_ms(100);
   }
 }
@@ -135,4 +143,94 @@ void read_sensor(float *data_array){
   // z acceleration
   int16_t zgyro_byte = ((sensor_data[12] << 8) | sensor_data[13]);
   data_array[6] = zgyro_byte*0.007630;
+}
+
+static void send_hid_report(uint8_t report_id, float xaccel)
+{
+  // skip if hid is not ready yet
+  if ( !tud_hid_ready() ) return;
+
+  switch(report_id)
+  {
+    case REPORT_ID_KEYBOARD:
+    {
+      // use to avoid send multiple consecutive zero report for keyboard
+      static bool has_keyboard_key = false;
+
+      if ( xaccel > 0.8 )
+      {
+        uint8_t keycode[6] = { 0 };
+        keycode[0] = HID_KEY_A;
+
+        tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, keycode);
+        has_keyboard_key = true;
+      }else
+      {
+        // send empty key report if previously has key pressed
+        if (has_keyboard_key) tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
+        has_keyboard_key = false;
+      }
+    }
+    break;
+
+    // case REPORT_ID_MOUSE:
+    // {
+    //   int8_t const delta = 5;
+
+    //   // no button, right + down, no scroll, no pan
+    //   tud_hid_mouse_report(REPORT_ID_MOUSE, 0x00, delta, delta, 0, 0);
+    // }
+    // break;
+
+    // case REPORT_ID_CONSUMER_CONTROL:
+    // {
+    //   // use to avoid send multiple consecutive zero report
+    //   static bool has_consumer_key = false;
+
+    //   if ( btn )
+    //   {
+    //     // volume down
+    //     uint16_t volume_down = HID_USAGE_CONSUMER_VOLUME_DECREMENT;
+    //     tud_hid_report(REPORT_ID_CONSUMER_CONTROL, &volume_down, 2);
+    //     has_consumer_key = true;
+    //   }else
+    //   {
+    //     // send empty key report (release key) if previously has key pressed
+    //     uint16_t empty_key = 0;
+    //     if (has_consumer_key) tud_hid_report(REPORT_ID_CONSUMER_CONTROL, &empty_key, 2);
+    //     has_consumer_key = false;
+    //   }
+    // }
+    // break;
+
+    // case REPORT_ID_GAMEPAD:
+    // {
+    //   // use to avoid send multiple consecutive zero report for keyboard
+    //   static bool has_gamepad_key = false;
+
+    //   hid_gamepad_report_t report =
+    //   {
+    //     .x   = 0, .y = 0, .z = 0, .rz = 0, .rx = 0, .ry = 0,
+    //     .hat = 0, .buttons = 0
+    //   };
+
+    //   if ( btn )
+    //   {
+    //     report.hat = GAMEPAD_HAT_UP;
+    //     report.buttons = GAMEPAD_BUTTON_A;
+    //     tud_hid_report(REPORT_ID_GAMEPAD, &report, sizeof(report));
+
+    //     has_gamepad_key = true;
+    //   }else
+    //   {
+    //     report.hat = GAMEPAD_HAT_CENTERED;
+    //     report.buttons = 0;
+    //     if (has_gamepad_key) tud_hid_report(REPORT_ID_GAMEPAD, &report, sizeof(report));
+    //     has_gamepad_key = false;
+    //   }
+    // }
+    // break;
+
+    default: break;
+  }
 }
